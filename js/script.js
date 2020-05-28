9fe94fb215cba4f490219ab9a6c201110ba6b059
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-05-27 23:08:21 -0400

    Added notification field to let user know if workday is over

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@
 document.getElementById("header-date").textContent = moment().format("MMM DD, YYYY");
 
 // Log current hour
-var currentHour = moment().format("H");
+let currentHour = moment().format("H");
+let currentHourUS = moment().format("h");
+let amPm = currentHour > 11 ? "pm" : "am";
 let timeslots = document.getElementsByClassName("event");
+let eventList = {};
 
 // if(currentHour > 7 && currentHour < 19){
 //     if(currentHour < getElementsByClassName(".event"))
@@ -18,5 +21,6 @@ for(i=8; i < 19; i++){
         document.getElementById(`${i}h`).style = "background-color: #2C4251; color: white; border-right: 3px solid white;";
     } if(currentHour > i){
         document.getElementById(`${i}h`).style = "background-color: #ebebeb; color: black;";
+        document.getElementById("notification").textContent = `It's ${currentHourUS}.00${amPm}. Your workday is over and all events are completed. Check back tomorrow!`;
     }
 }
