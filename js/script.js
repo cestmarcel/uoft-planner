7a4a3d946297883bda66e32da22319b6aebc6d33
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-05-27 22:31:20 -0400

    Included JavaScript to dynamically color timeslots

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@ document.getElementById("header-date").textContent = moment().format("MMM DD, YY
 
 // Log current hour
 var currentHour = moment().format("H");
+let timeslots = document.getElementsByClassName("event");
 
-if(currentHour > 8 && currentHour < 19){
+// if(currentHour > 7 && currentHour < 19){
+//     if(currentHour < getElementsByClassName(".event"))
+// } if(currentHour >= 19){
 
-} else {
-    
+// }
+
+for(i=8; i < 19; i++){
+    if(currentHour < i){
+        document.getElementById(`${i}h`).style = "background-color: #fafafa; color: black;";
+    } if(currentHour == i){
+        document.getElementById(`${i}h`).style = "background-color: #2C4251; color: white; border-right: 3px solid white;";
+    } if(currentHour > i){
+        document.getElementById(`${i}h`).style = "background-color: #ebebeb; color: black;";
+    }
 }
